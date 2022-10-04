3e347ce67313ec4173dfc008f55809e365cfdacc
Camilo López <camiloj.lopez79@gmail.com>
2022-10-04 09:38:50 -0300

    Created services and contact pages

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Pages
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 import { PagesComponent } from './pages.component';
 import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PagesComponent,
     children: [
-      // TODO: Config children
-      { path: 'products', component: ProductsComponent }
+      // TODO: Config Lazyload
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'contact', component: ContactComponent }
     ]
   }
-]
-
+];
 
 @NgModule({
   exports: [
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,20 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 // Components
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { PagesComponent } from './pages.component';
 import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
 // Modules
 import { ComponentsModule} from '../components/components.module';
 import { SharedModule } from '../shared/shared.module';
 
-
-
 @NgModule({
   declarations: [
+    ContactComponent,
     HomeComponent,
     PagesComponent,
-    ProductsComponent
+    ProductsComponent,
+    ServicesComponent,
   ],
   imports: [
     CommonModule,
