d7d6b105da9a25a3456da2f8148e8df511bac9bd
Camilo López <camiloj.lopez79@gmail.com>
2022-10-04 09:30:57 -0300

    Implemented RouterModule on Navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public menuItems = [
+    {
+      name: 'Inicio',
+      url: '/'
+    },
+    {
+      name: 'Productos',
+      url: 'products'
+    },
+    {
+      name: 'Servicios',
+      url: 'services'
+    },
+    {
+      name: 'Contacto',
+      url: 'contact'
+    }
+  ]
+
 
   constructor() { }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 // Components
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -16,7 +17,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
