152eff1e5b42aacb90a92802fa79bf2de61cde94
Camilo López <camiloj.lopez79@gmail.com>
2022-10-17 08:38:14 -0300

    Implemented marker on Mapbox on contact

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { Map, Marker } from 'mapbox-gl';
+import { LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -7,6 +7,7 @@ import { Map, Marker } from 'mapbox-gl';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements AfterViewInit {
+  public staticLnLat: LngLatLike = [-70.4087393, -23.6710097]
   @ViewChild( 'mapDiv' ) mapDivElement!: ElementRef;
 
   constructor() { 
@@ -16,9 +17,14 @@ export class MapComponent implements AfterViewInit {
     const map = new Map({
       container: this.mapDivElement.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-70.4087393, -23.6710097], // starting position [lng, lat]
+      center: this.staticLnLat, // starting position [lng, lat]
       zoom: 14, // starting zoom
     });
-
+    const marker = new Marker({
+      color: "#E0698D",
+      draggable: true
+    }).setLngLat( this.staticLnLat )
+      .setPopup(new Popup().setHTML( "<h2 class='my-3'>Hair by Eve</h2>" ))
+      .addTo( map );
   }
 }
